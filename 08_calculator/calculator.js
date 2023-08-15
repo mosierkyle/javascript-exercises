34fd285eff3085528c688705b041888d6ccc1c22
Kyle Mosier <mosierkyle@gmail.com>
2023-08-15 15:51:17 -0700

    Create calculator functions
    Created a calculator with the function guidelines given.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return (num1) + (num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return (num1) - (num2)
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let nums = args
+  return nums.reduce((total, item) => {return total += item}, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let nums = args
+  return nums.reduce((total, item) => {return total *= item}, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return (num1) ** (num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let fact = 1;
+  for(let i = num1; i > 0; i--) {
+    fact *= i;
+  }
+  return fact
 };
 
 // Do not edit below this line
