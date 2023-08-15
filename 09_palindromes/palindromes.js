105e13b0afbe5967de0a677e005046e90646f2ae
Kyle Mosier <mosierkyle@gmail.com>
2023-08-15 16:35:31 -0700

    Create palindromes checker function
    Created a palindrome checker function that takes a string and sees if its a palindrome excluding punctuation, spaces, etc.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringArr = Array.from(string.toLowerCase());
 
+    let noSpace = stringArr.filter(item => {return item !== ' ' && item !== '.' && item !== ',' && item !== '!'})
+
+    let newArr = noSpace.reverse();
+
+    let ogString = stringArr.filter(item => {return item !== ' ' && item !== '.' && item !== ',' && item !== '!'})
+
+    for(let i = 0; i < newArr.length; i++) {
+      if(newArr[i] !== ogString[i]){
+        return false
+      }
+    }
+    
+    return true
 };
 
 // Do not edit below this line
